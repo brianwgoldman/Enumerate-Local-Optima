2dab9f06c256d2d172ae7dd069b62655d4da8ec5
Goldman <brianwgoldman@acm.org>
2015-08-10 12:36:48 -0400

    Expanded make_all to include larger non-SAT problems

--- make_all.py
+++ make_all.py
@@ -9,7 +9,7 @@ target_K = {"DeceptiveTrap" : 5, "MAXSAT" : 3, "IsingSpinGlass" : 2,
             "AdjacentNKq" : 3, "RandomNKq" : 3}
 
 for problem_name in make_mk.standard_problems:
-    for N in range(15, 51):
+    for N in range(15, 71):
         for seed in range(30):
             K = target_K[problem_name]
             if problem_name == "DeceptiveTrap":
@@ -17,5 +17,8 @@ for problem_name in make_mk.standard_problems:
                     continue
             if problem_name == "IsingSpinGlass":
                 if int(math.sqrt(N)) ** 2 != N:
+                    continue
+            if problem_name == "MAXSAT":
+                if N > 50:
                     continue
             make_mk.create(folder, problem_name, N, K, seed)
