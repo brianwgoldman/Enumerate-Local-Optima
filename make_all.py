69946c41b5e5ac07b51a2681b3bc4e77d3d3fc8d
Brian W. Goldman <brianwgoldman@acm.org>
2016-01-09 11:59:06 -0500

    Added more comments to the Python scripts, should be ready for paper release

--- make_all.py
+++ make_all.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python3
+'''
+This file recreates all MK Landscapes used by the
+`Hyperplane Elimination for Quickly Enumerating Local Optima`
+paper. Requies a command line argument for the output folder.
+'''
 import make_mk
 import sys
 import math
 
+# Takes the output folder as a command line argument
 folder = sys.argv[1]
 
-target_K = {"DeceptiveTrap" : 5, "MAXSAT" : 3, "IsingSpinGlass" : 2,
+# Specifies the k used for each problem
+target_k = {"DeceptiveTrap" : 5, "MAXSAT" : 3, "IsingSpinGlass" : 2,
             "AdjacentNKq" : 3, "RandomNKq" : 3}
 
 for problem_name in make_mk.standard_problems:
     for N in range(15, 101):
         for seed in range(30):
-            K = target_K[problem_name]
+            k = target_k[problem_name]
             if problem_name == "DeceptiveTrap":
-                if N % K != 0:
+                if N % k != 0:
+                    # N must be evenly divisible by k for this problem.
                     continue
             if problem_name == "IsingSpinGlass":
                 if int(math.sqrt(N)) ** 2 != N:
+                    # 2D Spin Glasses require N be a power of 2
                     continue
             if problem_name == "MAXSAT":
                 if N > 50:
+                    # Big SATs failed horribly, so I stopped generating them.
                     continue
-            make_mk.create(folder, problem_name, N, K, seed)
+            make_mk.create(folder, problem_name, N, k, seed)
+# Create the extra large Adjacent NKq
 for seed in range(30):
     make_mk.create(folder, "AdjacentNKq", 200, 3, seed)
